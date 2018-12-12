f843f544c89359c6cef1303ea3b2221ae5c588c6
CeriseAndres <cerise.andres@yahoo.fr>
2018-12-12 09:49:49 +0100

    fix

--- pong_cherry.py
+++ pong_cherry.py
@@ -1,15 +1,9 @@
 from sense_hat import SenseHat
 
-
-import pygame
-from pygame.locals import *
-
-pygame.init()
-
 sense = SenseHat()
 sense.clear()
 
-ball_x=0
-ball_y=0
+ball_x=5
+ball_y=3
 
-ball_move= sense.set_pixels(ball_x,ball_y)
+ball_move=sense.set_pixel(ball_x,ball_y,255,255,255)
